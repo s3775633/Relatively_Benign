541a850150d49df763415f586f637f26c7a549c7
LewyMarto1 <s3775633@student.rmit.edu.au>
2020-04-08 15:24:31 +1000

    Added Dummy items for testing and set up inventory

---
+++ Gauge/Assets/Project Scripts/Interaction.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public GameObject currentObject = null;
+    public InteractionObject currentInterObjScript = null;
+    public Inventory inventory;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactive_Object"))
+        {
+            Debug.Log(other.name);
+            currentObject = other.gameObject;
+            currentInterObjScript = currentObject.GetComponent<InteractionObject>();
+        }
+        if (currentObject)
+        {
+            if (currentInterObjScript.inventory)
+            {
+                inventory.AddItem(currentObject);
+            }
+            currentObject.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (currentObject == GameObject.Find("Shotgun"))
+        {
+            Debug.Log("Current Weapon is Shotgun");
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Gauge/Assets/Project Scripts/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject[] inventory = new GameObject[5];
+
+    void Start()
+    {
+        inventory[0] = GameObject.Find("Pistol");
+    }
+    
+    public void AddItem(GameObject item)
+    {
+        if (SearchInventory(item))
+        {
+            for (int x = 0; x < inventory.Length; x++)
+            {
+                if (inventory[x] == null)
+                {
+                    inventory[x] = item;
+                    Debug.Log(item.name + " was Added");
+                    break;
+                }
+            }
+        }
+    }
+
+    public bool SearchInventory(GameObject item)
+    {
+        for(int x = 0; x < inventory.Length; x++)
+        {
+            if(inventory[x] == item)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
