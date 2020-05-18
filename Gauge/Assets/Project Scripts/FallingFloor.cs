b42bd3e1c3bc8fc4a21cab47076d7faa5cb2cf3d
BlakeGordon-RMIT <55981311+BlakeGordon-RMIT@users.noreply.github.com>
2020-05-18 21:19:54 +1000

    Add files via upload
    Uploaded falling floor script

---
+++ Gauge/Assets/Project Scripts/FallingFloor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFloor : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        // set the collision variable
+        public Collider2D triggered;
+        public Animator animator;
+        public AudioSource falling_trap_audio;
+        public bool activated;
+        public float timer = 1.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            triggered = this.GetComponent<Collider2D>();
+            animator = this.GetComponent<Animator>();
+            falling_trap_audio = this.GetComponent<AudioSource>();
+            activated = false;
+        }
+
+        private void Update()
+        {
+            if(activated == true)
+            {
+                timer -= Time.deltaTime;
+            }
+
+            if (timer <0 )
+            {
+                animator.SetBool("active", true);
+                
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                activated = true;
+                falling_trap_audio.Play();
+
+        }
+        }
+
+        
+    }
