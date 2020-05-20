94221ac7cffb722525f82ce5e76ad7cde18b2694
Mogcode <s3301010@student.rmit.edu.au>
2020-05-21 01:51:06 +0800

    Implimented AudioManager
    Created an AudioManager to crossfade music tracks when an enemy is encountered.
    Added a script to test the features of the AudioManager (keys 1-7)
    Added weapon sounds and a pickup sound to be used for keys.

---
+++ Gauge/Assets/AudioManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Static Instance
+
+    private static AudioManager instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<AudioManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject("Create AudioManager", typeof(AudioManager)).GetComponent<AudioManager>();
+                }
+            }
+
+            return instance;
+        }
+        private set
+        {
+            instance = value;
+        }
+    }
+    #endregion
+
+    #region Fields
+    private AudioSource musicSource;
+    private AudioSource musicSource2;
+    private AudioSource sfxSource;
+
+    private bool firstMusicSourceIsPlaying;
+    #endregion
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        musicSource = this.gameObject.AddComponent<AudioSource>();
+        musicSource2 = this.gameObject.AddComponent<AudioSource>();
+        sfxSource = this.gameObject.AddComponent<AudioSource>();
+
+        // Loop the music tracks
+        musicSource.loop = true;
+        musicSource2.loop = true;
+
+    }
+
+    public void PlayMusic(AudioClip musicClip)
+    {   // Determine which source is active
+        AudioSource activeSource = (firstMusicSourceIsPlaying) ? musicSource : musicSource2;
+
+        activeSource.clip = musicClip;
+        activeSource.volume = 1;
+        activeSource.Play();
+    }
+    public void PlayMusicWithFade(AudioClip newClip, float transitionTime = 1.0f)
+    {
+        // Determine which source is active
+        AudioSource activeSource = (firstMusicSourceIsPlaying) ? musicSource : musicSource2;
+
+        StartCoroutine(UpdateMusicWithFade(activeSource, newClip, transitionTime));
+    }
+    public void PlayMusicWithCrossFade(AudioClip musicClip, float transitionTime = 1.0f)
+    {
+        // Determine which source is active
+        AudioSource activeSource = (firstMusicSourceIsPlaying) ? musicSource : musicSource2;
+        AudioSource newSource = (firstMusicSourceIsPlaying) ? musicSource2 : musicSource;
+
+        // Swap the source
+        firstMusicSourceIsPlaying = !firstMusicSourceIsPlaying;
+
+        // Set the fields of the audio source, then start the coroutine to crossfade
+        newSource.clip = musicClip;
+        newSource.Play();
+        StartCoroutine(UpdateMusicWithCrossFade(activeSource, newSource, transitionTime));
+    }
+
+    private IEnumerator UpdateMusicWithFade(AudioSource activeSource, AudioClip newClip, float transitionTime)
+    {
+        // Make sure the source is active and playing
+        if (!activeSource.isPlaying)
+            activeSource.Play();
+
+        // Fade out
+        for (float t = 0.0f; t < transitionTime; t += Time.deltaTime)
+        {
+            activeSource.volume = (1 - (t / transitionTime));
+            yield return null;
+        }
+
+        activeSource.Stop();
+        activeSource.clip = newClip;
+        activeSource.Play();
+
+        // Fade in
+        for (float t = 0.0f; t < transitionTime; t += Time.deltaTime)
+        {
+            activeSource.volume = (t / transitionTime);
+            yield return null;
+        }
+    }
+    private IEnumerator UpdateMusicWithCrossFade(AudioSource original, AudioSource newSource, float transitionTime)
+    {
+
+        for (float t = 0.0f; t < transitionTime; t += Time.deltaTime)
+        {
+            original.volume = (1 - (t / transitionTime));
+            newSource.volume = (t / transitionTime);
+            yield return null;
+        }
+        original.Stop();
+
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+    // overload for PlaySFX
+    public void PlaySFX(AudioClip clip, float volume)
+    {
+        sfxSource.PlayOneShot(clip, volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+        musicSource2.volume = volume;
+    }
+    public void SetSfxVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+}
---
+++ Gauge/Assets/AudioManagerTester.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * Quick overview:
+ * you can cycle through the various sound modes using the keys 1-7 (above qwerty)
+ * crossfade can be altered to any float value.
+ */
+public class AudioManagerTester : MonoBehaviour
+{
+    [SerializeField] private AudioClip SoundEffect;
+    [SerializeField] private AudioClip music1;
+    [SerializeField] private AudioClip music2;
+
+    private void Start()
+    {
+        AudioManager.Instance.PlayMusic(music2);
+    }
+
+    private void Update()
+    {
+        // Play soundeffect
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            AudioManager.Instance.PlaySFX(SoundEffect);
+
+        // Play music track 1
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            AudioManager.Instance.PlayMusic(music1);
+
+        // Play music track 2
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            AudioManager.Instance.PlayMusic(music2);
+
+        // Play music track 1 with fade
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            AudioManager.Instance.PlayMusicWithFade(music1);
+
+        // Play music track 2 with fade
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            AudioManager.Instance.PlayMusicWithFade(music2);
+
+        // Play music track 1 with a crossfade of 3 seconds
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            AudioManager.Instance.PlayMusicWithCrossFade(music1, 3.0f);
+
+        // play music track 2 with a crossfade of 3 seconds
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+            AudioManager.Instance.PlayMusicWithCrossFade(music2, 3.0f);
+    }
+}
